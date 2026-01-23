6032e66d5250460b8706cb24a800b31e90dfe538
Guilherme Romanowski Barbosa <guilherme_romanowski_@hotmail.com>
2026-01-23 07:52:20 -0300

    Conclusão da aula 02

--- dia01/03_Series.index.py
+++ dia01/03_Series.index.py
@@ -30,4 +30,35 @@ series_idades.iloc[0] #primeira posição
 series_idades.iloc[-1] #última posição
 
 #O .iloc basicamente ignora as chaves e busca direto pelo elemento, como numa lista.
-series_idades.iloc[:3] 
+series_idades.iloc[:3]
+series_idades.iloc[::-1] 
+
+#%% Pordemos também setar os índices
+idades = [32, 38, 30, 30, 31,
+          35, 25, 29, 31, 37,
+          27, 23, 36, 33, 39]
+
+indexs = ["Teo", "Maria", "Jose", "Luis", "Ana",
+          "Nah", "Dani", "Mah", "Fer", "Nanda",
+          "Naty", "Nih", "Pedro", "Kozato", "Tito"]
+
+series_idades = pd.Series(idades,index=indexs)
+series_idades
+
+
+# %%Assim as chaves passam a ser o nome do index.
+series_idades["Teo"]
+# E continuamos podendo trazer a posição utilizando o .iloc
+series_idades.iloc[0]
+# %%
+import pandas as pd
+idades = [32, 38, 30, 30, 31,
+          35, 25, 29, 31, 37,
+          27, 23, 36, 33, 39]
+
+nomes = ["Teo", "Maria", "Jose", "Luis", "Ana",
+          "Nah", "Dani", "Mah", "Fer", "Nanda",
+          "Naty", "Nih", "Pedro", "Kozato", "Tito"]
+
+Series_idades = pd.Series(idades)
+Series_nomes = pd.Series(nomes)
